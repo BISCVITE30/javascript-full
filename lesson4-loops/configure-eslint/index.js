9fb345d81ddb168d9ed8bbb82420a6ccfe0475d2
BISCVITE30 <gonnki2004@gmail.com>
2024-02-19 09:40:11 +0100

    finish some tasks

--- lesson4-loops/configure-eslint/index.js
+++ lesson4-loops/configure-eslint/index.js
@@ -8,6 +8,7 @@ function run() {
   for (let i = 1; i < 10; i+= 1) {
     console.log(i);
   }
+  return 0;
 }
 
 run();
---
+++ lesson4-loops/sum-numbers/index.js
@@ -0,0 +1,6 @@
+const n = 150;
+let i = 0;
+while (i < n) {
+  i += 1;
+  console.log(i);
+}
