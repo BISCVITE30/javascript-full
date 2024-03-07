786a74743c7677fe619c38248222b2c881d921d3
BISCVITE30 <gonnki2004@gmail.com>
2024-03-07 10:23:44 +0100

    practice with tests

--- lesson13-modules-and-unit-tests/first-test-with-jest/index.test.js
+++ lesson13-modules-and-unit-tests/first-test-with-jest/index.test.js
@@ -1,3 +1,14 @@
 it('17 и в Африке 17', () => {
   expect(17).toEqual(17);
 });
+
+it('17 это вам не 18', () => {
+  expect(17).not.toEqual(18);
+});
+
+const getEvenNumber = arr => arr.filter(num => num % 2 === 0);
+
+it('should get even number from array', () => {
+  const result = getEvenNumber([1, 2, 3, 4, 5, 6]);
+  expect(result).toEqual([2, 4, 6]);
+});
