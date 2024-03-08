46dcf7a2f3412b47b85a97a53f0542b1b54fa0e6
BISCVITE30 <gonnki2004@gmail.com>
2024-03-08 09:36:53 +0100

    add coma

---
+++ lesson14-lexical-environmant-scope/inner-function/index.js
@@ -0,0 +1,19 @@
+function createMessenger() {
+  const message = 'Just learn it';
+  const sender = 'Gromcode';
+
+  // put your code here
+}
+
+// examples
+const messanger1 = createMessenger();
+messanger1.sendMessage('Anna'); // ===> Hello, Anna! Just learn it! This message was sent by Gromcode
+
+const messanger2 = createMessenger();
+messanger2.setMessage('You are learning JS and you do it well');
+messanger2.sendMessage('Michael'); // ===> Hello, Michael! You are learning JS and you do it well! This message was sent by Gromcode
+
+const messanger3 = createMessenger();
+messanger3.setMessage('The weather is amazing today');
+messanger3.setSender('Anna');
+messanger3.sendMessage('Alex'); // ===> Hello, Alex! The weather is amazing today! This message was sent by Anna
--- lesson14-lexical-environmant-scope/lexical-environmant/index.js
+++ lesson14-lexical-environmant-scope/lexical-environmant/index.js
@@ -2,7 +2,7 @@ let message = 'Just learn it';
 
 export function sendMessage(name) {
   const sender = 'Your Gromcode';
-  console.log(`${name} ${message}! ${sender}`);
+  console.log(`${name}, ${message}! ${sender}`);
 }
 
 export function setMessage(text) {
