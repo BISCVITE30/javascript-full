008b8789f9dbf889676736426aa8bbe4b6024664
BISCVITE30 <gonnki2004@gmail.com>
2024-03-19 09:53:50 +0100

    1

--- lesson21-document-object-model/finding-element-on-page/index.js
+++ lesson21-document-object-model/finding-element-on-page/index.js
@@ -1,10 +1,10 @@
-export function getTitleElement() {
-  const titleElem = document.querySelector('title');
+function getTitleElement() {
+  const titleElem = document.querySelector('h1[class="title"]');
   console.dir(titleElem);
   return titleElem;
 }
 
-export function getInputElement() {
+function getInputElement() {
   const inputElem = document.querySelector('input[type="text"]');
   console.dir(inputElem);
   return inputElem;
